a6a75f634dc2fabbdd377fdf631ef26c14aac498
Hannes Achleitner <hannes.software@gmx.at>
2025-09-28 07:41:07 +0200

    rule: delete_movie

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def toEncoder(file_description):
 
 @app.route('/files', methods=['GET'])
 def listFile():
-    print("I do >listFile<", request.method)
+    print("I do >/files<", request.method)
     if request.method == 'GET':
         files = os.listdir(UPLOAD_FOLDER)
         file_descriptors = sorted(list(map(toFileDescriptor, files)), key=operator.attrgetter('created'), reverse=True)
@@ -46,83 +46,43 @@ def listFile():
     return ''
 
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    print("I do a ", request.method)
-    print("---headers---\r\n", request.headers)
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('No file part')
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            sub_url = ""
-            if os.path.isfile('parameter.yml'):
-                with open('parameter.yml', 'r') as file:
-                    prime_service = yaml.safe_load(file)
-                    if __name__ == '__main__':
-                        sub_url = prime_service['server']['sub_url']
-            print("sub_url=", sub_url)
-
-            return redirect(sub_url + url_for('download_file', name=filename))
+@app.route('/avi', methods=['GET'])
+def listAvi():
+    print("I do >/avi<", request.method)
+    if request.method == 'GET':
+        files = os.listdir(UPLOAD_FOLDER)
+        listed = list(map(toFileDescriptor, files))
+        filtered_listed = [s for s in listed if str(s.name).lower().endswith(".avi")]
+        file_descriptors = sorted(filtered_listed, key=operator.attrgetter('created'), reverse=True)
+        encoded_file_descriptors = list(map(toEncoder, file_descriptors))
+        return jsonify(results=encoded_file_descriptors)
     return ''
 
 
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+@app.route('/movies/<filetype>', methods=['GET'])
+def list_movies(filetype):
+    print("I do >/movies/" + filetype + "<", request.method)
+    if request.method == 'GET':
+        files = os.listdir(UPLOAD_FOLDER)
+        listed = list(map(toFileDescriptor, files))
+        filtered_listed = [s for s in listed if str(s.name).lower().endswith(str(filetype).lower())]
+        file_descriptors = sorted(filtered_listed, key=operator.attrgetter('created'), reverse=True)
+        encoded_file_descriptors = list(map(toEncoder, file_descriptors))
+        return jsonify(results=encoded_file_descriptors)
+    return request.method + ' not supported'
 
 
-@app.route('/get', methods=['GET'])
-def get():
-    name = request.args['name']
-    try:
-        fp = open(UPLOAD_FOLDER + '/' + name, 'r')
-        content = fp.read()
-        fp.close()
-        return content
-    except FileNotFoundError:
-        print("Please check the path")
+@app.route('/deletemovie/<filetype>/<name>', methods=['GET'])
+def delete_movie(filetype, name):
+    print("I do >/deletemovie/" + filetype + "/" + name + "<", request.method)
 
+    if os.path.exists(name):
+        os.remove(name)
+        print(f"File '{name}' deleted successfully.")
+    else:
+        print(f"File '{name}' not found.")
 
-@app.route('/create', methods=['POST'])
-def create():
-    name = request.form['name']
-
-    fp = open(UPLOAD_FOLDER + '/' + name, 'w')
-    fp.write('content of ' + name)
-    fp.close()
-    return redirect('/')
-
-
-@app.route('/update', methods=['POST'])
-def update():
-    old_name = request.form['old_name']
-    new_name = request.form['new_name']
-    os.rename(UPLOAD_FOLDER + '/' + old_name, UPLOAD_FOLDER + '/' + new_name)
-    return redirect('/')
-
-
-@app.route('/delete', methods=['POST'])
-def delete():
-    name = request.form['name']
-    os.remove(UPLOAD_FOLDER + '/' + name)
-    return redirect('/')
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return list_movies(filetype)
 
 
 if __name__ == '__main__':
