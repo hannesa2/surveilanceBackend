f0789162d76e2469b4614a5bacef7e2234d61fc9
Hannes Achleitner <hannes.software@gmx.at>
2025-09-28 14:44:57 +0200

    Use webcam subdirectory

--- main.py
+++ main.py
@@ -25,31 +25,42 @@ def index():
     return render_template('index.html', items=os.listdir(UPLOAD_FOLDER))
 
 
-def toFileDescriptor(filename):
-    states = os.stat(str(Path.cwd()) + "/" + UPLOAD_FOLDER + "/" + filename)
-    return FileDescription(filename, states.st_size, states.st_mtime)
+def withWebCam(filename, webcam_name):
+    return webcam_name + "/" + filename
+
+
+def toFileDescriptor(absolute_filename):
+    states = os.stat(absolute_filename)
+    web_cam = str(absolute_filename).split('/')[-2]
+    file_name = str(absolute_filename).split('/')[-1]
+    return FileDescription(web_cam + "/" + file_name, states.st_size, states.st_mtime)
 
 
 def toEncoder(file_description):
     return FileDescriptionEncoder().encode(file_description)
 
 
-@app.route('/files', methods=['GET'])
-def listFile():
-    print("I do >/files<", request.method)
+def absolute_file_paths(directory):
+    path = os.path.abspath(directory)
+    return [entry.path for entry in os.scandir(path) if entry.is_file()]
+
+
+@app.route('/files/<webcam>', methods=['GET'])
+def listFile(webcam):
+    print("I do >/files/" + webcam + "<", request.method)
     if request.method == 'GET':
-        files = os.listdir(UPLOAD_FOLDER)
+        files = absolute_file_paths(UPLOAD_FOLDER + "/" + webcam)
         file_descriptors = sorted(list(map(toFileDescriptor, files)), key=operator.attrgetter('created'), reverse=True)
         encoded_file_descriptors = list(map(toEncoder, file_descriptors))
         return jsonify(results=encoded_file_descriptors)
     return ''
 
 
-@app.route('/avi', methods=['GET'])
-def listAvi():
-    print("I do >/avi<", request.method)
+@app.route('/avi/<webcam>', methods=['GET'])
+def listAvi(webcam):
+    print("I do >/avi/" + webcam + "<", request.method)
     if request.method == 'GET':
-        files = os.listdir(UPLOAD_FOLDER)
+        files = absolute_file_paths(UPLOAD_FOLDER + "/" + webcam)
         listed = list(map(toFileDescriptor, files))
         filtered_listed = [s for s in listed if str(s.name).lower().endswith(".avi")]
         file_descriptors = sorted(filtered_listed, key=operator.attrgetter('created'), reverse=True)
@@ -58,11 +69,11 @@ def listAvi():
     return ''
 
 
-@app.route('/movies/<filetype>', methods=['GET'])
-def list_movies(filetype):
-    print("I do >/movies/" + filetype + "<", request.method)
+@app.route('/movies/<filetype>/<webcam>', methods=['GET'])
+def list_movies(filetype, webcam):
+    print("I do >/movies/" + filetype + "," + webcam + "<", request.method)
     if request.method == 'GET':
-        files = os.listdir(UPLOAD_FOLDER)
+        files = absolute_file_paths(UPLOAD_FOLDER + "/" + webcam)
         listed = list(map(toFileDescriptor, files))
         filtered_listed = [s for s in listed if str(s.name).lower().endswith(str(filetype).lower())]
         file_descriptors = sorted(filtered_listed, key=operator.attrgetter('created'), reverse=True)
@@ -71,15 +82,15 @@ def list_movies(filetype):
     return request.method + ' not supported'
 
 
-@app.route('/deletemovie/<filetype>/<name>', methods=['GET'])
-def delete_movie(filetype, name):
-    print("I do >/deletemovie/" + filetype + "/" + name + "<", request.method)
-
-    if os.path.exists(name):
-        os.remove(name)
-        print(f"File '{name}' deleted successfully.")
+@app.route('/deletemovie/<webcam>/<filetype>/<name>', methods=['GET'])
+def delete_movie(webcam, filetype, name):
+    print("I do >/deletemovie/" + webcam + "/" + filetype + "/" + name + "<", request.method)
+    complete_name = webcam + "/" + name
+    if os.path.exists(complete_name):
+        os.remove(complete_name)
+        print(f"File '{complete_name}' deleted successfully.")
     else:
-        print(f"File '{name}' not found.")
+        print(f"File '{complete_name}' not found.")
 
     return list_movies(filetype)
 
